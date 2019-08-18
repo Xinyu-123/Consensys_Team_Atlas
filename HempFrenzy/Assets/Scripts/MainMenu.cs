3457122137a5a5b573d3a94ef233b81dad484905
Xinyu-123 <xinyudong1@gmail.com>
2019-08-17 20:08:12 -0400

    Level One Mostly Done

---
+++ HempFrenzy/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayStory()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void PlayInfinite()
+    {
+        SceneManager.LoadScene("InfiniteMode");
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+
+    public void Rules()
+    {
+        Debug.Log("Press Space to Jump. Collect Coins");
+    }
+    
+
+}
---
+++ HempFrenzy/Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject[] spawns;
+    void Start()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            Instantiate(spawns[0], new Vector3(10*i + 5, (float) 3, 0), Quaternion.identity);
+            Instantiate(spawns[1], new Vector3(10*i + (float)8.5, 4, 0), Quaternion.identity);
+            Instantiate(spawns[1], new Vector3(10 * i + (float) 8.9, 4, 0), Quaternion.identity);
+            Instantiate(spawns[1], new Vector3(10 * i +(float) 9.3, 4, 0), Quaternion.identity);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnThunder()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(spawns[2], new Vector3(10 * i + 5, (float)3, 0), Quaternion.identity);
+        }
+    }
+}
